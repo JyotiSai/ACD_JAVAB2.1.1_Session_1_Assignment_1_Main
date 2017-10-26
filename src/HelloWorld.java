4a9b34855377d930cdbe4dd31d344a53dc0ee8b6
JyotiSai <kajal.jojo@gmail.com>
2017-10-27 00:19:18 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
 
-public class HelloWorld
+public class HelloWorld // A class called HelloWorld is created with public access.
 {
-	public static void main(String[] args)
+	public static void main(String[] args) // Execution starts here; this function returns no value.
 	{
-		System.out.println("Hello World");
+		System.out.println("Hello World"); // Prints Hello World
 
 	}
 
